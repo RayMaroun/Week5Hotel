1e6dd03a0a2aa4f0296269ae2d5817f1d4b40450
RayMaroun <raymondmaroun15@gmail.com>
2026-05-07 19:38:01 +0300

    created the `Staff` class and the constructor and implemented the getters and setters and added a performDuty method

---
+++ src/main/java/com/pluralsight/Staff.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Staff {
+    private String name;
+    private String position;
+    private int id;
+
+    public Staff(String name, String position, int id) {
+        this.name = name;
+        this.position = position;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String performDuty(String dutyDescription){
+        return this.name + " is performing duty: " + dutyDescription;
+    }
+}
